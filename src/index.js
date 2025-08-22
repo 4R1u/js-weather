a0563a7b2fb9a17655293f6310745a8cfe84916e
4R1u <62248461+4R1u@users.noreply.github.com>
2025-08-22 06:19:47 +0400

    fix: implement weatherFetcher.today()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+const weatherFetcher = (function () {
+    const mainUrl = function (location, unitGroup = "metric", dates = "", elements = "", include = "") {
+        return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}${dates}?key=GYDSKL9BJ6CVEFMJ7E4YR7SRL&unitGroup=${unitGroup}&include=${include}&elements=${elements}`;
+    };
+
+    const today = function (location) {
+        fetch(mainUrl(location, "metric", "/today", "tempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Cpressure%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days%2Chours"), { mode: "cors" })
+            .then(function (response) { return response.json(); })
+            .then(function (response) { console.log(response); });
+    };
+
+    return { today, };
+})();
