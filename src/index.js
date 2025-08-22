a22700766e29bd45e206b3f919742ab2449b3496
4R1u <62248461+4R1u@users.noreply.github.com>
2025-08-22 08:44:56 +0400

    fix: implement weatherFetcher.tenday(location)

--- src/index.js
+++ src/index.js
@@ -17,7 +17,13 @@ const weatherFetcher = (function () {
             .then(function (response) { console.log(response); });
     };
 
-    return { today, hourly, };
+    const tenday = function (location) {
+        fetch(mainUrl(location, "metric", "/next10days", "datetime%2Ctemp%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
+            .then(function (response) { return response.json(); })
+            .then(function (response) { console.log(response); });
+    };
+
+    return { today, hourly, tenday, };
 })();
 
-weatherFetcher.hourly("Lahore");
+weatherFetcher.tenday("Lahore");
