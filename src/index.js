455f4564e20a15e66c4ac670f80404f7f2e4fe30
4R1u <62248461+4R1u@users.noreply.github.com>
2025-08-22 12:03:22 +0400

    feat: change all the fetch functions to return instead of print

--- src/index.js
+++ src/index.js
@@ -6,34 +6,38 @@ const weatherFetcher = (function () {
     };
 
     const today = function (location) {
-        fetch(mainUrl(location, "metric", "/today", "tempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Cpressure%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days%2Chours"), { mode: "cors" })
+        return fetch(mainUrl(location, "metric", "/today", "tempmax%2Ctempmin%2Ctemp%2Cfeelslike%2Cdew%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Cpressure%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days%2Chours"), { mode: "cors" })
             .then(function (response) { return response.json(); })
-            .then(function (response) { console.log(response); });
+            .then(function (response) { return response; });
     };
 
     const hourly = function (location) {
-        fetch(mainUrl(location, "metric", "/next24hours", "datetime%2Ctemp%2Cfeelslike%2Chumidity%2Cprecipprob%2Ccloudcover%2Cuvindex%2Cconditions%2Cdescription%2Cicon", "hours"), { mode: "cors" })
+        return fetch(mainUrl(location, "metric", "/next24hours", "datetime%2Ctemp%2Cfeelslike%2Chumidity%2Cprecipprob%2Ccloudcover%2Cuvindex%2Cconditions%2Cdescription%2Cicon", "hours"), { mode: "cors" })
             .then(function (response) { return response.json(); })
-            .then(function (response) { console.log(response); });
+            .then(function (response) { return response; });
     };
 
     const tenday = function (location) {
-        fetch(mainUrl(location, "metric", "/next10days", "datetimetempmax%Ctempmin%2Ctemp%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
+        return fetch(mainUrl(location, "metric", "/next10days", "datetimetempmax%Ctempmin%2Ctemp%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
             .then(function (response) { return response.json(); })
-            .then(function (response) { console.log(response); });
+            .then(function (response) { return response; });
     };
 
     const weekend = function (location) {
-        fetch(mainUrl(location, "metric", "/next14days", "datetimetempmax%Ctempmin%2Ctemp%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
+        return fetch(mainUrl(location, "metric", "/next14days", "datetimetempmax%Ctempmin%2Ctemp%2Chumidity%2Cprecipprob%2Cwindspeed%2Cwinddir%2Ccloudcover%2Cvisibility%2Cuvindex%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
             .then(function (response) { return response.json(); })
-            .then(function (response) { console.log(response); });
+            .then(function (response) { return response; });
     };
 
     const monthly = function (location) {
-        fetch(mainUrl(location, "metric", "/last38days/next38days", "datetime%2Ctempmax%2Ctempmin%2Cprecipprob%2Cwindspeed%2Cwinddir%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
+        return fetch(mainUrl(location, "metric", "/last38days/next38days", "datetime%2Ctempmax%2Ctempmin%2Cprecipprob%2Cwindspeed%2Cwinddir%2Csunrise%2Csunset%2Cmoonphase%2Cconditions%2Cdescription%2Cicon", "days"), { mode: "cors" })
             .then(function (response) { return response.json(); })
-            .then(function (response) { console.log(response); });
+            .then(function (response) { return response; });
     };
 
     return { today, hourly, tenday, weekend, monthly };
 })();
+
+weatherFetcher.today("Lahore")
+    .then(console.log);
+
