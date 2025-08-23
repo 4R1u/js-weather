005e434c199bcffd89233e3f6cd0912e9ed8f7f0
4R1u <62248461+4R1u@users.noreply.github.com>
2025-08-23 14:13:27 +0400

    feat: add basic today information

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 
 const weatherFetcher = (function () {
     const mainUrl = function (location, unitGroup = "metric", dates = "", elements = "", include = "") {
-        return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}${dates}?key=GYDSKL9BJ6CVEFMJ7E4YR7SRL&unitGroup=${unitGroup}&include=${include}&elements=${elements}`;
+        return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}${dates}?key=GYDSKL9BJ6CVEFMJ7E4YR7SRL&unitGroup=${unitGroup}&include=${include}&elements=${elements}&iconsSet=icons2`;
     };
 
     const today = function (location) {
@@ -45,13 +45,37 @@ const displayController = (function (doc) {
         event.preventDefault();
         mode(doc.querySelector("#location").value)
             .then(function (result) {
-                doc.querySelector(".temp").textContent = result["days"][0]["temp"];
+                doc.querySelector(".body").textContent = result["days"][0]["temp"];
             });
+    }
+
+    const loadToday = async function (event) {
+        event.preventDefault();
+        const result = await weatherFetcher.today(doc.querySelector("#location").value);
+        console.log(result);
+        doc.querySelector(".body").innerHTML = `
+        <div class="icon">${result["days"][0]["icon"]}</div>
+        <div class="address">${result["resolvedAddress"]}<div>
+        <div class="temp">${result["days"][0]["temp"]}<div>
+        <div class="conditions">${result["days"][0]["conditions"]}<div>
+        <div class="high">High ${result["days"][0]["tempmax"]}<div>
+        <div class="low">Low ${result["days"][0]["tempmin"]}<div>
+        <div class="feelslike">Feels Like ${result["days"][0]["feelslike"]}<div>
+        <div class="sunrise">Sunrise ${result["days"][0]["sunrise"]}<div>
+        <div class="sunset">Sunset ${result["days"][0]["sunset"]}<div>
+        <div class="wind">Wind: ${result["days"][0]["windspeed"]} km/h at ${result["days"][0]["winddir"]}<div>
+        <div class="humiditiy">Humidity: ${result["days"][0]["humidity"]}%<div>
+        <div class="dew-point">Dew point: ${result["days"][0]["dew"]}<div>
+        <div class="pressure">Pressure: ${result["days"][0]["pressure"]}<div>
+        <div class="uv-index">UV Index: ${result["days"][0]["uvindex"]}<div>
+        <div class="visibility">Visibility: ${result["days"][0]["visibility"]}<div>
+        <div class="moonphase">Moon Phase: ${result["days"][0]["moonphase"]}<div>
+        `;
     }
 
     doc.querySelector("form button").addEventListener("click", loadSearchedLocation);
 
-    doc.querySelector(".header-tab-today").addEventListener("click", () => { mode = weatherFetcher.today });
+    doc.querySelector(".header-tab-today").addEventListener("click", loadToday);
     doc.querySelector(".header-tab-hourly").addEventListener("click", () => { mode = weatherFetcher.hourly });
     doc.querySelector(".header-tab-tenday").addEventListener("click", () => { mode = weatherFetcher.tenday });
     doc.querySelector(".header-tab-weekend").addEventListener("click", () => { mode = weatherFetcher.weekend });
